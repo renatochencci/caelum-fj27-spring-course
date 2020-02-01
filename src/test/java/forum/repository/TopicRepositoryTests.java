ac6ebee4c441cda65318d4e0ed3d7ffc3cbdbfb6
Renato Chencci <renato.games.123@gmail.com>
2020-02-01 00:06:06 -0200

    sharing my first project from Spring Framework,1

---
+++ src/test/java/forum/repository/TopicRepositoryTests.java
@@ -0,0 +1,49 @@
+package forum.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.alura.forum.model.OpenTopicByCategory;
+import br.com.alura.forum.repository.TopicRepository;
+import forum.repository.setup.TopicRepositoryTestsSetup;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("dev")
+public class TopicRepositoryTests {
+	
+	@Autowired
+	private TopicRepository topicRepository;
+	
+	@Autowired
+	private TestEntityManager testEntityManager;
+	
+	@Test
+	public void shouldReturnOpenTopicsByCategory() {
+		TopicRepositoryTestsSetup testSetup = new TopicRepositoryTestsSetup(testEntityManager);
+        testSetup.openTopicsByCategorySetup();
+
+        List<OpenTopicByCategory> openTopics = this.topicRepository.findOpenTopicsByCategory();
+
+        assertThat(openTopics).isNotEmpty();
+        assertThat(openTopics).hasSize(2);
+
+        assertThat(openTopics.get(0).getCategoryName()).isEqualTo("Programação");
+        assertThat(openTopics.get(0).getTopicCount()).isEqualTo(2);
+
+        assertThat(openTopics.get(1).getCategoryName()).isEqualTo("Front-end");
+        assertThat(openTopics.get(1).getTopicCount()).isEqualTo(1);
+	}
+	
+}
